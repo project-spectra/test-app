d672b9105d0c1ff2650dcd5cedf6a782a15c1fcf
ozzmotic <ozzmotic@gmail.com>
2018-10-29 14:06:33 -0400

    replace with vue-cli template

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# test-app
-Putting together a test app by working through Nativescript tutorials. Hopefully will be able to record audio!
+# NativeScript-Vue Application
+
+> A native application built with NativeScript-Vue
+
+## Usage
+
+``` bash
+# Install dependencies
+npm install
+
+# Build for production
+tns build <platform> --bundle
+
+# Build, watch for changes and debug the application
+tns debug <platform> --bundle
+
+# Build, watch for changes and run the application
+tns run <platform> --bundle
+```
---
+++ app/components/App.vue
@@ -0,0 +1,137 @@
+<template>
+  <Page class="page">
+    <ActionBar title="Vue Audio Test" class="action-bar" />
+    
+    <TabView height="100%">
+    
+      <TabViewItem title="Record">
+        <StackLayout>
+          <Button text="Start Recording" @tap="start" />
+          <Button text="Stop Recording" @tap="stop" />
+          <Button class="btn btn-primary" :text="isPlaying ? 'Pause' : 'Play'" @tap="playPause" />
+        </StackLayout>
+      </TabViewItem>
+      
+      <TabViewItem title="Play">
+        <StackLayout>
+          <!-- <Progress :value="progress" /> -->
+
+        </StackLayout>
+      </TabViewItem>
+    
+    </TabView>
+  </Page>
+</template>
+
+<script>
+  var fs = require('file-system');
+  var permissions = require('nativescript-permissions');
+  var audio = require('nativescript-audio');
+
+  var recorder;
+  var audioFolder = fs.knownFolders.currentApp().getFolder('audio');
+  var recordingPath = audioFolder.path + '/recording.mp3';
+
+  var player = new audio.TNSPlayer();
+
+  var playerOptions = {
+    audioFile: recordingPath,
+    loop: false,
+    completeCallback: function() {
+        console.log('finished playing');
+    },
+    errorCallback: function(errorObject) {
+        console.log(JSON.stringify(errorObject));
+    },
+    infoCallback: function(args) {
+        console.log(JSON.stringify(args));
+    }
+  };
+
+  export default {
+    data: () => ({
+        msg: 'Hello World!',
+        progress: 0,
+        isPlaying: false,
+        isRecording: false,
+    }),
+    mounted() {
+
+    },
+    destroyed() {
+
+    },
+    methods: {
+      start() {
+        console.log('start button pressed');
+          
+          recorder = new audio.TNSRecorder();
+
+          if (audio.TNSRecorder.CAN_RECORD()) {
+
+            var recorderOptions = {
+              filename: recordingPath,
+              infoCallback: function () {
+                console.log('infoCallback');
+              },
+              errorCallback: function () {
+                console.log('errorCallback');
+                alert('Error recording.');
+              }
+            };
+
+            console.log('Recorder options: ' + recorderOptions);
+
+            recorder.start(recorderOptions).then(function(res) {
+              this.isRecording = true;
+            }, function(err) {
+              this.isRecording = false;
+              console.log('ERROR: ' + err);
+            });
+          } else {
+            alert('This device cannot record audio.');
+          }
+      },
+
+      stop() {
+        if (recorder != undefined) {
+        recorder.stop().then(function () {
+          this.isRecording = false;
+          console.log('Audio Recorded Successfully.');
+        }, function (err) {
+          console.log(err);
+          this.isRecording = true;
+        });
+        }
+      },
+
+      playPause() {
+        if (player.isAudioPlaying()) {
+          player.pause();
+        } else {
+          player.playFromFile(playerOptions)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log("Player error", err);
+        });
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
