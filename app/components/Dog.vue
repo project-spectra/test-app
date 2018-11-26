a90e749e9df718c6a1c6141abfdc820bb20352f0
ozzmotic <ozzmotic@gmail.com>
2018-11-26 16:23:22 -0500

    Add Big Dog Small Dog page and image

--- app/components/Dog.vue
+++ app/components/Dog.vue
@@ -1,18 +1,38 @@
 <template>
-    <Page>
-      <ActionBar title="Big Dog Small Dog"/>
+  <Page>
+    <ActionBar title="Big Dog Small Dog"/>
       <StackLayout>
         <Label text="Hello World" />
+        <Image id="dogImg" src="~/assets/images/dog.jpg" stretch="none" @tap="animateDog" />
       </StackLayout>
-    </Page>
+  </Page>
 </template>
 
 <script>
+import { AnimationCurve } from 'ui/enums';
+
 export default {
-    data() {
-	    return {
-	    };
+    data: () => ({
+
+    }),
+    mounted() {
+    },
+  methods: {
+    animateDog(args) {
+      dogImg.animate({
+        duration: 3000,
+        curve: AnimationCurve.easeInOut,
+        scale: { x: 2, y: 2}
+      }).then(() => {
+        // Reset animation
+        setTimeout(() => {
+            view.scaleX = 1;
+            view.scaleY = 1;
+        }, 1000)
+    })
     }
+  }
+
 }
 </script>
 
