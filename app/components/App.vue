1db9aef48a57952cfb9e3db39e53b76c13b4b393
Alex <ozzmotic@gmail.com>
2019-01-24 14:59:04 -0500

    Remove audio recorder functions from main App component

--- app/components/App.vue
+++ app/components/App.vue
@@ -14,11 +14,6 @@
         </GridLayout>
         
         <Label text="Recorder" class="label font-weight-bold m-b-5" />
-        <!--
-        <Button text="Start Recording" @tap="start" />
-        <Button text="Stop Recording" @tap="stop" />
-        <Button class="btn btn-primary" :text="isPlaying ? 'Pause' : 'Play'" @tap="playPause" />
-        -->
         <audio-recorder></audio-recorder>
 
         <Label text="Exercises" class="label font-weight-bold m-b-5" />
@@ -32,39 +27,12 @@
   import Dog from '@/components/Dog'
   import Convo from '@/components/Convo'
   import AudioRecorder from '@/components/AudioRecorder'
-  import * as ApplicationSettings from "application-settings";
-
-  var fs = require('file-system');
-  var permissions = require('nativescript-permissions');
-  var audio = require('nativescript-audio');
-
-  var recorder;
-  var audioFolder = fs.knownFolders.currentApp().getFolder('audio');
-  var recordingPath = audioFolder.path + '/recording.mp3';
-
-  var player = new audio.TNSPlayer();
-
-  var playerOptions = {
-    audioFile: recordingPath,
-    loop: false,
-    completeCallback: function() {
-        console.log('finished playing');
-    },
-    errorCallback: function(errorObject) {
-        console.log(JSON.stringify(errorObject));
-    },
-    infoCallback: function(args) {
-        console.log(JSON.stringify(args));
-    }
-  };
 
   export default {
     components: { AudioRecorder },
     data: () => ({
         msg: 'Hello World!',
         progress: 0,
-        isPlaying: false,
-        isRecording: false,
         input: {
           name: "",
           firstLoad: true,
@@ -82,65 +50,6 @@
     },
 
     methods: {
-
-      //Recorder methods
-      start() {
-        console.log('start button pressed');
-          
-          recorder = new audio.TNSRecorder();
-
-          if (audio.TNSRecorder.CAN_RECORD()) {
-
-            var recorderOptions = {
-              filename: recordingPath,
-              infoCallback: function () {
-                console.log('infoCallback');
-              },
-              errorCallback: function () {
-                console.log('errorCallback');
-                alert('Error recording.');
-              }
-            };
-
-            console.log('Recorder options: ' + recorderOptions);
-
-            recorder.start(recorderOptions).then(function(res) {
-              this.isRecording = true;
-            }, function(err) {
-              this.isRecording = false;
-              console.log('ERROR: ' + err);
-            });
-          } else {
-            alert('This device cannot record audio.');
-          }
-      },
-
-      stop() {
-        if (recorder != undefined) {
-        recorder.stop().then(function () {
-          this.isRecording = false;
-          console.log('Audio Recorded Successfully.');
-        }, function (err) {
-          console.log(err);
-          this.isRecording = true;
-        });
-        }
-      },
-
-      playPause() {
-        if (player.isAudioPlaying()) {
-          player.pause();
-        } else {
-          player.playFromFile(playerOptions)
-          .then((res) => {
-            console.log(res);
-          })
-          .catch((err) => {
-            console.log("Player error", err);
-        });
-        }
-      },
-
       //Vuex methods
       save() {
         this.$store.commit("save", this.input);
