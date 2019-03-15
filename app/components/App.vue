a4ccd93291d5bc80774a884b2c6fa79be05c2ebf
Alex <ozzmotic@gmail.com>
2019-03-15 12:40:26 -0400

    Add dialog box functionality, and add template page for Stemple exercise

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,25 +1,37 @@
 <template>
-  <Page class="page">
+  <Page class="page" :class="{ dialogOpen: dialogOpen }">
     <ActionBar title="Project Spectra" class="action-bar" />
-      <StackLayout>
-        <StackLayout class="input-field">
-          <Label text="Name" class="label font-weight-bold m-b-5" />
-          <TextField class="input" hint="What is your name?" autocorrect="false" v-model="input.name" />
-          <StackLayout class="hr-light"></StackLayout>
-        </StackLayout>
-        <GridLayout rows="auto, auto" columns="*, *">
-          <Button text="Save" @tap="save" class="btn btn-primary" row="0" col="0" />
-          <Button text="Load" @tap="load" class="btn btn-primary" row="0" col="1" />
-          <Button text="clear" @tap="clear" class="btn btn-primary" row="1" col="0" colSpan="2" />
-        </GridLayout>
-        
-        <Label text="Recorder" class="label font-weight-bold m-b-5" />
-        <audio-recorder></audio-recorder>
+      <GridLayout>
 
-        <Label text="Exercises" class="label font-weight-bold m-b-5" />
-        <Button text="Big Dog Small Dog" @tap="goToDog" />
-        <Button text="Conversation" @tap="goToConvo" />
-      </StackLayout>
+        <StackLayout class="content p-20">
+          <StackLayout class="input-field">
+            <Label id="greeting" v-model="welcomeText" />
+            <TextField v-if="noName" class="input" hint="What is your name?" autocorrect="false" v-model="input.name"></TextField>
+          </StackLayout>
+          <GridLayout v-if="noName" id="nameControls" rows="auto, auto" columns="*, *">
+            <Button text="Save" @tap="save" class="btn btn-primary" row="0" col="0" />
+            <Button text="Load" @tap="load" class="btn btn-primary" row="0" col="1" />
+            <Button text="clear" @tap="clear" class="btn btn-primary" row="1" col="0" colSpan="2" />
+          </GridLayout>
+          
+          <Label text="Recorder" class="label font-weight-bold m-b-5" />
+          <!-- <audio-recorder></audio-recorder> !-->
+
+          <Label text="Exercises" class="label font-weight-bold m-b-5" />
+          <Button text="Big Dog Small Dog" @tap="goToDog" />
+          <Button text="Conversation" @tap="goToConvo" />
+          <Button text="Dialog test" @tap="showDialog" />
+        </StackLayout>
+
+        <!-- Welcome Dialog !-->
+        <AbsoluteLayout class="dialog-wrapper">
+          <StackLayout class="dialog">
+            <Label class="h3" textWrap="true" text="Welcome + Explain purpose of app" />
+            <Button class="btn btn-primary" text="Next" @tap="closeDialog" />
+          </StackLayout>
+        </AbsoluteLayout>
+
+      </GridLayout>
   </Page>
 </template>
 
@@ -27,22 +39,37 @@
   import Dog from '@/components/Dog'
   import Convo from '@/components/Convo'
   import AudioRecorder from '@/components/AudioRecorder'
+  import * as ApplicationSettings from "application-settings";
 
   export default {
     components: { AudioRecorder },
-    data: () => ({
+    data () {
+      return {
         msg: 'Hello World!',
         progress: 0,
         input: {
           name: "",
           firstLoad: true,
-        }
-    }),
+        },
+        noName: true,
+        dialogOpen: false,
+        welcomeText: "Welcome to Spectra!",
+      };
+    },
+
     mounted() {
       this.$store.subscribe((mutations, state) => {
         ApplicationSettings.setString("store", JSON.stringify(state));
+        
         this.input.name = state.name;
         this.input.firstLoad = state.firstLoad;
+        
+        if (state.name === "") {
+          this.welcomeText = "Welcome to Spectra!";          
+        } else {
+          this.welcomeText = "Welcome back, " + state.name + "!";
+          this.noName = false;
+        }
       });
     },
     destroyed() {
@@ -60,6 +87,15 @@
       clear() {
         this.input.name = "";
         this.input.firstLoad = true;
+        this.noName = true;
+      },
+
+      //Dialog methods
+      showDialog() {
+        this.dialogOpen = true;
+      },
+      closeDialog() {
+        this.dialogOpen = false;
       },
 
       //Navigation methods
@@ -74,15 +110,55 @@
 </script>
 
 <style scoped>
-    ActionBar {
-        background-color: #53ba82;
-        color: #ffffff;
+  @keyframes show {
+    from {
+      opacity: 0;
     }
+    to {
+      opacity: 1;
+    }
+  }
 
-    .message {
-        vertical-align: center;
-        text-align: center;
-        font-size: 20;
-        color: #333333;
-    }
+  .dialogOpen .content,
+  .dialogOpen .action-bar {
+    opacity: 0.2;
+  }
+
+  .dialogOpen .dialog-wrapper {
+    visibility: visible;
+    animation-name: show;
+    animation-duration: 0.3s;
+    animation-fill-mode: forwards;
+  }
+
+	.dialog-wrapper {
+		visibility: collapse;
+		opacity: 0;
+	}
+
+	.dialog {
+		border-width: 1 0 1 0;
+		border-color: black;
+		background-color: white;
+		width: 100%;
+		margin-top: 100;
+		padding: 20;
+	}
+
+	.dialog Label {
+		margin: 0 15 15 15;
+		color: black;
+	}
+
+  ActionBar {
+      background-color: #53ba82;
+      color: #ffffff;
+  }
+
+  .message {
+      vertical-align: center;
+      text-align: center;
+      font-size: 20;
+      color: #333333;
+  }
 </style>
---
+++ app/components/Stemple.vue
@@ -0,0 +1,31 @@
+<template>
+  <Page ref="page">
+    <ActionBar title="Stemple Exercise"/>
+      <StackLayout>
+        <Label text="" textWrap="true"/>
+
+      </StackLayout>
+  </Page>
+</template>
+
+<script>
+import * as animation from "tns-core-modules/ui/animation";
+var enums = require("tns-core-modules/ui/enums");
+var dogImg;
+
+export default {
+    data: () => ({
+
+    }),
+    mounted() {
+
+    },
+  methods: {
+
+  }
+
+}
+</script>
+
+<style>
+</style>
