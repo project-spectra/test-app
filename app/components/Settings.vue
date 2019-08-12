b5f6090270162662779e128dece082aea76d8782
ozzmotic <ozzmotic@gmail.com>
2019-08-12 10:35:28 -0400

    Create settings buttons

---
+++ app/components/Settings.vue
@@ -0,0 +1,76 @@
+<template>
+  <Page actionBarHidden="true" class="page">
+    <FlexboxLayout style="flex: 1;"
+    flexDirection="column" id="container">
+      <TextView :text="'Settings'"
+      editable="false" id="settings-title" />
+
+      <StackLayout orientation="vertical" horizontalAlignment="center" id="settings-buttons">
+        <SpectraActionButton text="Change your goals" style="width: 80%" type="settings-orange" @tap="onGoalChange "/>
+
+        <SpectraActionButton text="Change your name" style="width: 80%" type="settings-green" @tap="onNameChange"/>
+
+        <SpectraActionButton text="View credits" style="width: 80%" type="settings-orange" @tap="onCredits"/>
+
+        <SpectraActionButton text="Share feedback" style="width: 80%" type="settings-green" @tap="onFeedback"/>
+
+        <SpectraActionButton text="Back" style="width: 50%" type="settings-orange" @tap="onBack"/>
+      </StackLayout>
+    </FlexboxLayout>
+  </Page>
+</template>
+
+<script>
+  import SpectraActionButton from "./UIControls/SpectraActionButton";
+  import App from "./App";
+
+  export default {
+    components: {
+      SpectraActionButton
+    },
+    methods: {
+      onGoalChange() {
+
+      },
+      onNameChange() {
+
+      },
+      onCredits() {
+
+      },
+      onFeedback() {
+
+      },
+      onBack() {
+        this.$navigateTo(App);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  Page {
+    background-color: #E8E8E8;
+  }
+
+  #settings-buttons {
+    margin-top: 50;
+    margin-bottom: 50;
+  }
+
+  #container {
+    padding: 15em;
+  }
+
+  #settings-title {
+    font-size: 45em;
+    font-family: 'serif';
+    font-weight: 700;
+    color: #000;
+    border-width: 0;
+    background-color: transparent;
+    border-color: #E8E8E8;
+}
+
+
+</style>
