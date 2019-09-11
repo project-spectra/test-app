5a290a6ec7c7bde25132a732e63e84ed7258d23c
ozzmotic <ozzmotic@gmail.com>
2019-09-11 12:57:21 -0400

    Add disclaimer screen

--- app/components/OnboardingScreens/Welcome.vue
+++ app/components/OnboardingScreens/Welcome.vue
@@ -30,8 +30,7 @@
 <script>
     import SpectraTextView from "@/components/UIControls/SpectraTextView";
     import SpectraActionButton from "@/components/UIControls/SpectraActionButton";
-
-    import VocalHealthWarnings from "@/components/OnboardingScreens/VocalHealthWarnings";
+    import Disclaimer from "@/components/OnboardingScreens/Disclaimer";
 
     export default {
         components: {
@@ -53,7 +52,7 @@
                 this.$store.dispatch("setName", this.name);
                 this.$store.dispatch("setFirstLoad", true);
                 //this.$store.commit("save", {name: this.name, firstLoad: true});
-                this.$navigateTo(VocalHealthWarnings);
+                this.$navigateTo(Disclaimer);
             }
         }
     }
