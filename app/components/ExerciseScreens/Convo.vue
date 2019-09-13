2a0b571d0ee0c820eb56efd9f783367abf3325d5
Bryan Kok <bryan.wyern1@gmail.com>
2019-09-14 02:57:41 +0800

    Use arrow functions to resolve the self/this scope issue and the .stop() promise syntax issue

--- app/components/ExerciseScreens/Convo.vue
+++ app/components/ExerciseScreens/Convo.vue
@@ -1,28 +1,29 @@
 <template>
-    <Page actionBarHidden="true" class="page" >
-      <FlexboxLayout style="flex: 1;" flexDirection="column" id="container">
+    <Page actionBarHidden="true" class="page">
+        <FlexboxLayout style="flex: 1;" flexDirection="column" id="container">
             <TextView text="Conversation Practice" editable="false" id="title"/>
 
-            <TextView editable="false" :text="answering ? ANSWERING_TEXT : INFO_TEXT" height="150em" id="desc" />
+            <TextView editable="false" :text="answering ? ANSWERING_TEXT : INFO_TEXT" height="150em" id="desc"/>
 
-            <!--cat chat goes here !--> 
+            <!--cat chat goes here !-->
             <AbsoluteLayout height="200em" backgroundColor="lightgray" margin="10em">
 
-              <Image src="res://cat" left="200em" top="0" width="120em" stretch="aspectFit" />
+                <Image src="res://cat" left="200em" top="0" width="120em" stretch="aspectFit"/>
 
-              <TextView left="0" top="30dp" id="chat-bubble" editable="false" :text="question" />
+                <TextView left="0" top="30dp" id="chat-bubble" editable="false" :text="question"/>
 
-              <Textview left="70" top="100dp" id="chat-bubble" editable="false" text="I'm listening!" v-if="answering" />
+                <Textview left="70" top="100dp" id="chat-bubble" editable="false" text="I'm listening!"
+                          v-if="answering"/>
 
             </AbsoluteLayout>
 
-            <StackLayout flexGrow="1" />
+            <StackLayout flexGrow="1"/>
 
-            <!--nav buttons--> 
+            <!--nav buttons-->
             <FlexboxLayout justifyContent="space-between" flexDirection="row">
-                <SpectraActionButton type='warning' text="Back" @tap="onBack" />
+                <SpectraActionButton type='warning' text="Back" @tap="onBack"/>
 
-                <SpectraActionButton :text="answering ? 'Done' : 'Answer'" @tap="onStart" />
+                <SpectraActionButton :text="answering ? 'Done' : 'Answer'" @tap="onStart"/>
             </FlexboxLayout>
         </FlexboxLayout>
     </Page>
@@ -31,7 +32,7 @@
 <script>
 
     import SpectraActionButton from "@/components/UIControls/SpectraActionButton";
-    import { alert } from 'tns-core-modules/ui/dialogs/dialogs';
+    import {alert} from 'tns-core-modules/ui/dialogs/dialogs';
     import ConvoViz from '@/components/ExerciseScreens/ConvoViz';
 
     //Initialize recorder
@@ -40,90 +41,94 @@
     const audio = require('nativescript-audio');
     const audioFolder = fs.knownFolders.temp();
     const recordingPath = audioFolder.path + '/recording.mp3';
-    var recorder;
+    let recorder;
 
     export default {
-      components: {SpectraActionButton},
-      data() {
-          return {
+        components: {SpectraActionButton},
+        data() {
+            return {
                 INFO_TEXT:
                     "Let's put the skills you've learned into practice, and get a sense of where your voice is at. Respond to the question freely, as though you were talking to someone next to you. When you're ready, tap 'Answer'!",
                 ANSWERING_TEXT:
                     "Your voice is currently being recorded.\n\nWhen you're done responding to the question, tap 'Done!' and you will receive feedback on your voice.",
                 answering: false,
                 isRecording: false,
-          }
-      },
-      computed: {
-        question() {
-          //pick a random question from a list
-          var questions = Array(
-            "How is your day going?",
-            "What are you looking\nforward to right now?",
-            "What was the last\nfun thing you did?",
-            "Tell me about a close\nfriend or family member.",
-            "What's something you like\nto do in your free time?",
-            "What was the last movie\nyou saw? What did you think?",
-            "What's your favorite animal?\n(It's okay if it's not a cat!)"
-          )
-          return questions[Math.floor(Math.random()*questions.length)];
-        }
-      },
-      methods: {
-        onBack: function() {
-          this.$navigateBack();
+            }
         },
-        onStart: function() {
-          //If not answering yet
-          if (!this.answering) {
-            //Chat bubble appears with "I'm listening"
-            this.answering = true;
-            //Start the recording
-            recorder = new audio.TNSRecorder();
+        computed: {
+            question() {
+                //pick a random question from a list
+                let questions = Array(
+                    "How is your day going?",
+                    "What are you looking\nforward to right now?",
+                    "What was the last\nfun thing you did?",
+                    "Tell me about a close\nfriend or family member.",
+                    "What's something you like\nto do in your free time?",
+                    "What was the last movie\nyou saw? What did you think?",
+                    "What's your favorite animal?\n(It's okay if it's not a cat!)"
+                );
+                return questions[Math.floor(Math.random() * questions.length)];
+            }
+        },
+        methods: {
+            onBack: function () {
+                this.$navigateBack();
+            },
+            onStart: function () {
+                //If not answering yet
+                if (!this.answering) {
+                    //Chat bubble appears with "I'm listening"
+                    this.answering = true;
+                    //Start the recording
+                    recorder = new audio.TNSRecorder();
 
-            if (audio.TNSRecorder.CAN_RECORD()) {
-              var recorderOptions = {
-                filename: recordingPath,
-                infoCallback: function () {
-                  console.log('infoCallback');
-                },
-                errorCallback: function () {
-                  console.log('errorCallback');
-                  alert('Error recording.');
+                    if (audio.TNSRecorder.CAN_RECORD()) {
+                        let recorderOptions = {
+                            filename: recordingPath,
+                            infoCallback: function (args) {
+                                console.log('infoCb', JSON.stringify(args));
+                            },
+                            errorCallback: function () {
+                                console.log('errorCallback');
+                                alert('Error recording.');
+                            }
+                        };
+
+                        recorder.start(recorderOptions).then(function (res) {
+                            console.log("Recording has started to " + recordingPath);
+                            this.isRecording = true;
+                        }, function (err) {
+                            this.isRecording = false;
+                            console.log('ERROR: ' + err);
+                        });
+                    } else {
+                        alert('Unfortunately, this exercise requires a microphone.');
+                    }
+
+                } else { //If user is recording and taps "Done"
+                    console.log("Done tapped");
+                    //Stop recording
+                    if (recorder !== undefined) {
+                        recorder.stop().then(() => {
+                            this.isRecording = false;
+                            this.answering = false;
+                            console.log('Audio Recorded Successfully.');
+                        }).catch((err) => {
+                            console.log(err);
+                            console.log('--');
+                            console.log("Error");
+                            this.isRecording = true;
+                            this.answering = false;
+                        });
+                    }
+
+                    //Navigate to new page, do analysis of recording, show visualization, delete recording
+                    let exists = fs.File.exists(recordingPath);
+                    console.log("File exists: " + exists + " in " + fs.path.normalize(recordingPath));
+                    this.$navigateTo(ConvoViz, {props: {recPath: recordingPath, clearHistory: true}});
                 }
-              };
-
-              recorder.start(recorderOptions).then(function(res) {
-                console.log("Recording has started to " + recordingPath);
-                this.isRecording = true;
-              }, function(err) {
-                this.isRecording = false;
-                console.log('ERROR: ' + err);
-              });
-            } else {
-              alert('Unfortunately, this exercise requires a microphone.');
             }
-
-          } else { //If user is recording and taps "Done"
-            console.log("Done tapped");
-            //Stop recording
-            if (recorder != undefined) {
-              recorder.stop().then(function () {
-                this.isRecording = false;
-                console.log('Audio Recorded Successfully.');
-              }, function (err) {
-                console.log("Error");
-                this.isRecording = true;
-              });
-            }
-
-            //Navigate to new page, do analysis of recording, show visualization, delete recording
-            var exists = fs.File.exists(recordingPath);
-            console.log("File exists: " + exists + " in " + fs.path.normalize(recordingPath))
-            this.$navigateTo(ConvoViz, {props: {recPath: recordingPath, clearHistory: true}});
-          }
         }
-      }
 
     }
 </script>
@@ -152,12 +157,12 @@
     }
 
     #chat-bubble {
-      background-color: white;
-      font-size: 15em;
-      border-radius: 8em;
-      border-width: 1em;
-      border-color: black;
-      padding: 7em;
+        background-color: white;
+        font-size: 15em;
+        border-radius: 8em;
+        border-width: 1em;
+        border-color: black;
+        padding: 7em;
     }
 
 </style>
